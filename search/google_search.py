ccb5324a0c9529b7cb698dc1a62daf8dc79523c0
adarsh78-rgb <adarshshwet@gmail.com>
2025-05-01 18:53:03 +0530

    google_search.py

--- search/google_search.py
+++ search/google_search.py
@@ -1,10 +1,3 @@
-import subprocess
-import sys
-
-try:
-    import serpapi
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "serpapi"])
 
 from serpapi import GoogleSearch
 import os
